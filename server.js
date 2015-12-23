3601a7e7f16ddadc43d40cfa5a5a76948952ffa7
Jake McCaig <jake@downparty.io>
2015-12-22 22:59:00 -0700

    Updated local IP

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ status.hourly();
 status.daily();
 status.weekly();
 
-server.listen(process.env.PORT || 3000, process.env.IP || "localhost", function(){
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Server listening at", addr.address + ":" + addr.port);
 });
