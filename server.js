e2732e51a4a55b9fe4844070689b4da87e82b4b8
Jake McCaig <jake@downparty.io>
2015-12-22 22:45:57 -0700

    Added daily and weekly logging

--- server.js
+++ server.js
@@ -37,6 +37,8 @@ io.on('connection', function (socket) {
 });
 
 status.hourly();
+status.daily();
+status.weekly();
 
 server.listen(process.env.PORT || 3000, process.env.IP || "localhost", function(){
   var addr = server.address();
